642259a20c3efe33cbe39146b5d539e435057165
HannahO94 <56817756+HannahO94@users.noreply.github.com>
2020-09-29 14:37:46 +0200

    Updates

---
+++ precache-manifest.023b2333759e506980ce4f0a994a8777.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ad77e98345fd771d47c26cc07b9ce6",
+    "url": "/fakestagram/index.html"
+  },
+  {
+    "revision": "30c3c6ee285fd6a98b0a",
+    "url": "/fakestagram/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "6225a480bf25583f2dc6",
+    "url": "/fakestagram/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "30c3c6ee285fd6a98b0a",
+    "url": "/fakestagram/static/js/2.4ca13c0c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fakestagram/static/js/2.4ca13c0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6225a480bf25583f2dc6",
+    "url": "/fakestagram/static/js/main.94ef053f.chunk.js"
+  },
+  {
+    "revision": "b366113173f51521abb1",
+    "url": "/fakestagram/static/js/runtime-main.d5d96a28.js"
+  }
+]);
